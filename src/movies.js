ef8a18cff251e31dc62a01669b6db68b063037ff
“Nicole <itsnicoleduffy@gmail.com>
2023-12-12 13:01:33 +0100

    updated format

--- src/movies.js
+++ src/movies.js
@@ -3,118 +3,94 @@
 // How could you "clean" a bit this array and make it unified (without duplicates)?
 
 function getAllDirectors(moviesArray) {
+  const directorsArr = moviesArray.map((oneMovie) => {
+    return oneMovie.director;
+  });
 
-    const directorsArr = moviesArray.map((oneMovie) => {
-        return oneMovie.director;
-    });
-
-    return directorsArr
+  return directorsArr;
 }
-
-
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies(moviesArray) {
+  if (moviesArray.length == 0) {
+    return 0;
+  }
 
-    if (moviesArray.length == 0) {
-        return 0;
-    }   
+  const dramaMoviesBySteven = moviesArray.filter((movie) => {
+    const moviesBySteven = movie.director === "Steven Spielberg";
+    const isMovieDrama = movie.genre.indexOf("Drama") >= 0;
 
-    const dramaMoviesBySteven = moviesArray.filter((movie) => {
-        const moviesBySteven = movie.director === "Steven Spielberg";
-        const isMovieDrama = movie.genre.indexOf("Drama") >= 0;
+    return moviesBySteven && isMovieDrama;
+  });
 
-        return moviesBySteven && isMovieDrama;
-    });
-    
-    return dramaMoviesBySteven.length;
+  return dramaMoviesBySteven.length;
 }
-
-
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(moviesArray) {
+  if (moviesArray.length === 0) {
+    return 0;
+  }
 
-    if (moviesArray.length === 0) {
-      return 0;  
-    } 
+  const sumOfScore = moviesArray.reduce((accumulator, movie) => {
+    if (movie.score) return accumulator + movie.score;
+    else {
+      return accumulator;
+    }
+  }, 0);
 
-    const sumOfScore = moviesArray.reduce((accumulator, movie) => {
-        if (movie.score)
-        return accumulator + movie.score;
-    
-        else{
-        return accumulator;
-       }
-    }, 0);
-
-    const averageScore = sumOfScore / moviesArray.length;
-    return +(averageScore.toFixed(2));
+  const averageScore = sumOfScore / moviesArray.length;
+  return +averageScore.toFixed(2);
 }
-
-
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore(moviesArray) {
+  const dramaMovies = moviesArray.filter((movie) => {
+    return movie.genre.includes("Drama");
+  });
 
-    const dramaMovies = moviesArray.filter((movie) => {
-        return movie.genre.includes("Drama");
-    });
-
-    averageScore = scoresAverage(dramaMovies);
-    return averageScore;
+  averageScore = scoresAverage(dramaMovies);
+  return averageScore;
 }
-
-
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(moviesArray) {
+  const deepCopyArr = JSON.parse(JSON.stringify(moviesArray));
+  const sorted = deepCopyArr.sort((a, b) => {
+    if (a.year > b.year) {
+      return 1;
+    } else if (a.year < b.year) {
+      return -1;
+    } else {
+      if (a.title > b.title) {
+        return 1;
+      } else if (a.title < b.title) {
+        return -1;
+      } else {
+        0;
+      }
+    }
+  });
 
-    const deepCopyArr = JSON.parse(JSON.stringify(moviesArray))
-    const sorted = deepCopyArr.sort((a, b) => {
-        if (a.year > b.year) {
-            return 1;
-        }
-        else if (a.year < b.year) {
-            return -1;
-        }
-        else {
-            if (a.title > b.title) {
-                return 1;
-            }
-            else if (a.title < b.title) {
-                return -1;
-            }
-            else {
-                0;
-            }
-        }
-    });
-
-    return sorted;
+  return sorted;
 }
-
-
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(moviesArray) {
+  const copyArr = JSON.parse(JSON.stringify(moviesArray));
+  const sortedAlpha = copyArr.sort((a, b) => {
+    if (a.title > b.title) {
+      return 1;
+    } else if (a.title < b.title) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
 
-    const copyArr = JSON.parse(JSON.stringify(moviesArray));
-    const sortedAlpha = copyArr.sort((a, b) => {
-        if (a.title > b.title){
-            return 1;
-        }
-        else if (a.title < b.title){
-            return -1;
-        }
-        else {
-            return 0;
-        }
-    })
-
-    const titlesArr = sortedAlpha.map((movie) => movie.title)
-    const first20 = titlesArr.slice(0, 20)
-    return first20;
+  const titlesArr = sortedAlpha.map((movie) => movie.title);
+  const first20 = titlesArr.slice(0, 20);
+  return first20;
 }
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
